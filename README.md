611010b99cca25c80661d27bf1a76f3d09391868
Kevin Christ <christkevin50@gmail.com>
2025-10-23 18:04:59 +0200

    added option to pass a flag with the location

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 Simple cli application to get the current temperature.
 This works by fetching data from the [OpenMeteoAPI](https://open-meteo.com/).
 
+![goweather](./goweather.png)
+
+## Usage
+
+You can simply run `go run main.go` and the programm gets 
+executed with "Berlin" as the default location.
+
+There is the `-location` flag.
+Run `go run main.go -location "<YOUR_LOCATION>"` and the [GeocodingAPI](https://open-meteo.com/en/docs/geocoding-api)
+will determine the coordinates for you and simply give you the temperature.
--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
 )
+
+type GeocodingResponse struct {
+	Results []struct {
+		Name      string  `json:"name"`
+		Latitude  float64 `json:"latitude"`
+		Longitude float64 `json:"longitude"`
+		Country   string  `json:"country"`
+	} `json:"results"`
+}
 
 type WeatherResponse struct {
 	Latitude  float64 `json:"latitude"`
@@ -20,7 +32,58 @@ type WeatherResponse struct {
 }
 
 func main() {
-	api := "https://api.open-meteo.com/v1/forecast?latitude=49.3064&longitude=8.4503&current=temperature_2m,weather_code&timezone=Europe%2FBerlin&forecast_days=1"
+	locationPtr := flag.String("location", "Berlin", "Location provided to get the coordinates")
+	flag.Parse()
+
+	location := *locationPtr
+	if location == "" {
+		fmt.Println("ERROR: Please enter a valid location with the -location Flag.")
+		os.Exit(1)
+	}
+
+	encodedLocation := url.QueryEscape(location)
+	geoAPIURL := fmt.Sprintf(
+		"https://geocoding-api.open-meteo.com/v1/search?name=%s&count=1&language=de&format=json",
+		encodedLocation,
+	)
+	fmt.Printf("1/2: Search for coordinates'%s'...\n", location)
+
+	geoResponse, err := http.Get(geoAPIURL)
+	if err != nil {
+		log.Fatalf("Error sending request: %v", err)
+	}
+	defer geoResponse.Body.Close()
+
+	if geoResponse.StatusCode != http.StatusOK {
+		log.Fatalf("Api call exited with the error code: %d", geoResponse.StatusCode)
+	}
+
+	geoBody, err := io.ReadAll(geoResponse.Body)
+	if err != nil {
+		log.Fatalf("Error parsing response: %v", err)
+	}
+
+	var geoData GeocodingResponse
+	if err := json.Unmarshal(geoBody, &geoData); err != nil {
+		log.Fatalf("Error parsing the json: %v", err)
+	}
+
+	if len(geoData.Results) == 0 {
+		log.Fatalf("No coordinates found for the location'%s'.", location)
+	}
+
+	// Koordinaten des ersten Ergebnisses extrahieren
+	lat := geoData.Results[0].Latitude
+	lon := geoData.Results[0].Longitude
+	foundName := geoData.Results[0].Name
+	fmt.Printf("-> Found: %s, Lat=%.2f, Lon=%.2f\n", foundName, lat, lon)
+
+	// cunstructing the url with the input
+	api := fmt.Sprintf(
+		"https://api.open-meteo.com/v1/forecast?latitude=%.2f&longitude=%.2f&current=temperature_2m,weather_code&timezone=Europe%%2FBerlin&forecast_days=1",
+		lat,
+		lon,
+	)
 
 	fmt.Printf("Sending request: %s\n", api)
 	response, err := http.Get(api)
@@ -36,7 +99,7 @@ func main() {
 
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
-		log.Fatalf("Error reading the response: %v", err)
+		log.Fatalf("Error parsing the response: %v", err)
 	}
 
 	var weatherData WeatherResponse
